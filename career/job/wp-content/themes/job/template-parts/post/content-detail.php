e14f0caeb9dd9862afc786451eed7d39db8e2d7b
smasuzawa <koubousei@gmail.com>
2018-11-07 11:24:05 +0900

    /career/job/～ 募集要項修正

--- career/job/wp-content/themes/job/template-parts/post/content-detail.php
+++ career/job/wp-content/themes/job/template-parts/post/content-detail.php
@@ -36,17 +36,37 @@
         
       </div>
       <div class="txt">
-    <div class="btnEntry topbtn">
-      <a href="/career/job/entry/index.php?jobtitle=<?php the_title(); ?>&office=<?php the_field('office'); ?>">エントリーはこちら</a>
-    </div>
+        <div class="btnEntry topbtn">
+          <a href="/career/job/entry/index.php?jobtitle=<?php the_title(); ?>&office=<?php the_field('office'); ?>">エントリーはこちら</a>
+        </div>
         <?php
-      /* translators: %s: Name of current post */
-      the_content( sprintf(
-        __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ),
-        get_the_title()
-      ) );
-
-      ?>
+          /* translators: %s: Name of current post */
+          the_content( sprintf(
+            __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ),
+            get_the_title()
+          ) );
+        ?>
+        <dl>
+          <dt>【募集職種】</dt>
+          <dd><?php the_field('jobtype'); ?></dd>
+          <dt>【勤務地】</dt>
+          <dd><?php the_field('office'); ?><br><?php the_field('area'); ?></dd>
+          <dt>【給与】</dt>
+          <dd><?php the_field('pay1'); ?><br><?php the_field('pay2'); ?></dd>
+          <dt>【雇用形態】</dt>
+          <dd>
+            <?php 
+              $cats = get_the_category();
+              $cat = $cats[0];
+              if($cat->parent){
+                $parent = get_category($cat->parent);
+                echo $parent->cat_name;
+              }else{
+                echo $cat->cat_name;
+              }
+            ?>
+          </dd>
+        </dl>
       </div>
     </div>
 
@@ -54,27 +74,31 @@
     <h2>募集要項</h2>
     <div class="job-table">
       <dl>
-        <dt>募集職種</dt>
+        <dt>仕事内容</dt>
         <dd>
-          <?php the_field('jobtype'); ?>
+          <?php the_field('naiyou'); ?>
+        </dd>
+      </dl>
+      <dl>
+        <dt>仕事の魅力</dt>
+        <dd>
+          <?php the_field('miryoku'); ?>
         </dd>
       </dl>
       <dl>
         <dt>給与</dt>
         <dd>
-          <?php the_field('pay1'); ?><br>
-          <?php the_field('pay2'); ?>
+          <?php the_field('pay1'); ?><br><?php the_field('pay2'); ?>
         </dd>
       </dl>
       <dl>
-        <dt>勤務地 </dt>
+        <dt>勤務地</dt>
         <dd>
-          <?php the_field('office'); ?><br>
-          <?php the_field('area'); ?>
+          <?php the_field('office'); ?><br><?php the_field('area'); ?>
         </dd>
       </dl>
       <dl>
-        <dt>最寄り駅 </dt>
+        <dt>アクセス</dt>
         <dd>
           <?php the_field('moyori'); ?>
         </dd>
